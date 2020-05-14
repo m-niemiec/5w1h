d9aa0ec64c56e486c6c77412a7d0764974d5767d
m-niemiec <michal.niemiec.it@gmail.com>
2020-05-15 01:00:10 +0200

    Added question answered function for questiondetails, more styling, new position for some buttons and informations, started working on editing forms.

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
 from .models import *
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 
 class AskAQuestionForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['question'].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Question
         fields = ['question']
@@ -26,3 +33,14 @@ class BestAnswerForm(ModelForm):
     class Meta:
         model = Answer
         fields = ['question_id', 'answer']
+
+
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        fields = ('username', 'password1', 'password2')
+        model = get_user_model()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Display Name'
